f3301b7e8c406c9df2992efaa63d037ee2935d54
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-01-21 14:46:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,15 +35,15 @@ def cocanb_or_english():
         for i in selected:
             result.append(output[temp:i])
             temp = i
-        a = ["a","ā","ă","ą","ä","æ","à","á","â","ã"]
+        a = ["a","ā","ă","ą","ä","à","á","â","ã"]
         e = ["e","ė","ę","ě","ĕ","è","é","ê","ë","ē"]
         i = ["i","ı","į","ī","ï","î","í","ì"]
-        o = ["o","œ","ø","õ","ô","ó","ò","ö"]
+        o = ["o","ø","õ","ô","ó","ò","ö"]
         u = ["u","ū","ů","ų","ü","ù","ú","û"]
         y = ["y","ý"]
         # add more diacritics
         result_without_diacritics = " ".join(result)
-        final_result = result_without_diacritics.replace("a", random.choice(a)).replace("e", random.choice(e)).replace("i", random.choice(i)).replace("o", random.choice(o)).replace("u", random.choice(u)).replace("y", random.choice(y))
+        final_result = result_without_diacritics.replace("a", random.choice(a)).replace("e", random.choice(e)).replace("i", random.choice(i)).replace("o", random.choice(o)).replace("u", random.choice(u)).replace("y", random.choice(y)).replace("ae", "æ").replace("oe", "œ")
         print(final_result)
     else:
         print("Please enter a valid input.")
