40fd81626ae169e3ef4cf502e63d156164e296dd
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-01-21 11:44:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-input("Would you like to translate Cocánb to English or English to Cocánb? Press[c/e]")
+import unidecode
 
-# TODO: add code
+choose_lang = input("Would you like to translate Cocánb to English or English to Cocánb? Press[c/e]")
+
+def cocanb_or_english()
+    if choose_lang == c:
+        string_with_diacritics = input("Please input your Cocánb phrase: ")
+        string = unidecode.unidecode(string_with_diacritics)
+        # do this
+    elif choose_lang == e:
+        string = input("Please input your English phrase: ")
+        # and this
+    else:
+        cocanb_or_english()
 
 input("Press any key to exit.")
