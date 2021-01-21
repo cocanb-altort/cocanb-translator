dc88d5ba493ce4abc08371a99374d8f5205d3cff
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-01-21 13:28:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-import unidecode
 import numpy
 
 choose_lang = input("Would you like to translate Cocánb to English or English to Cocánb? Press[c/e]")
 
-def cocanb_or_english()
-    if choose_lang == c:
-        string_with_diacritics = input("Please input your Cocánb phrase: ")
-        string = unidecode.unidecode(string_with_diacritics)
+def cocanb_or_english():
+    if choose_lang =="c":
         print("This doesn't work yet")
-    elif choose_lang == e:
+    elif choose_lang == "e":
         string = input("Please input your English phrase: ")
         string_list = string.split(" ")
         end_letters = []
         lengths = []
-        for i in string_list:
-            string_list[i] = string_list[i].split("")
+        i = 0
+        while i <= len(string_list) - 1:
+            string_list[i] = list(string_list[i])
             lengths.append(chr(ord('`')+len(string_list[i])))
             end_letters.append(string_list[i][-1])
+            del string_list[i][-1]
+            "".join(string_list[i]) # this no work
+            i = i + 1
         suffix = [None]*(len(end_letters)+len(lengths))
         suffix[::2] = end_letters
         suffix[1::2] = lengths
-        output = ''.join(string_list) + "non" + suffix
-        print(output)
+        print(string_list)
+        # output_list = string_list + ["non"] + suffixoutput = "".join(output_list)
+        # output = "".join(output_list)
+        # print(output)
     else:
         print("Please enter a valid input.")
         cocanb_or_english()
