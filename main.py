3a001f74ab754834da39b8bff6c9d95dd4d4c069
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-01-21 13:54:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def cocanb_or_english():
         end_letters = []
         lengths = []
         i = 0
+        new_string_list = []
         while i <= len(string_list) - 1:
             string_list[i] = list(string_list[i])
             lengths.append(chr(ord('`')+len(string_list[i])))
             end_letters.append(string_list[i][-1])
             del string_list[i][-1]
-            "".join(string_list[i]) # this no work
+            new_string_list.append("".join(string_list[i])) # this no work
             i = i + 1
         suffix = [None]*(len(end_letters)+len(lengths))
         suffix[::2] = end_letters
         suffix[1::2] = lengths
-        print(string_list)
-        # output_list = string_list + ["non"] + suffixoutput = "".join(output_list)
-        # output = "".join(output_list)
-        # print(output)
+        output_list = new_string_list + ["non"] + suffix
+        output = "".join(output_list)
+        print(output)
     else:
         print("Please enter a valid input.")
         cocanb_or_english()
