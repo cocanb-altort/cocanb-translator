99fa5c342ec2b36b927a236c3defd7408c5bfb4e
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-03-03 16:30:00 +0100

    w

--- add_diacritics.py
+++ add_diacritics.py
@@ -1,19 +1,19 @@
 import random
 
-a = ["a", "à", "á", "â", "ä"]
-e = ["e", "è", "é", "ê"]
-i = ["i", "ì", "í", "î"]
-o = ["o", "ò", "ó", "ô", "ö"]
-u = ["u", "ù", "ú", "û", "ü"]
-y = ["y", "ý"]
-g = ["g", "ğ"]
-c = ["c", "č", "ć"]
-s = ["s", "š", "ś"]
-z = ["z", "ž", "ź"]
-n = ["n", "ň"]
-r = ["r", "ř"]
-l = ["l", "ł"]
-d = ["d", "đ"]
+a_diacritics = ["a", "à", "á", "â", "ä"]
+e_diacritics = ["e", "è", "é", "ê"]
+i_diacritics = ["i", "ì", "í", "î"]
+o_diacritics = ["o", "ò", "ó", "ô", "ö"]
+u_diacritics = ["u", "ù", "ú", "û", "ü"]
+y_diacritics = ["y", "ý"]
+g_diacritics = ["g", "ğ"]
+c_diacritics = ["c", "č", "ć"]
+s_diacritics = ["s", "š", "ś"]
+z_diacritics = ["z", "ž", "ź"]
+n_diacritics = ["n", "ň"]
+r_diacritics = ["r", "ř"]
+l_diacritics = ["l", "ł"]
+d_diacritics = ["d", "đ"]
 
 def add_diacritics():
     pass
--- english_to_cocanb.py
+++ english_to_cocanb.py
@@ -1,11 +1,16 @@
 
 
 def cocanbise(string):
+    # split string into words
     initial_list = " ".split(string)
 
+    # count letters of each word
+    word_counts = []
+    for word in initial_list:
+        word_counts.append(len(word))
 
 def english_to_cocanb():
     initial_phrase = input("Please enter your English Phrase: ")
     sentences = ".".split(initial_phrase)
-    for i in sentences:
-        cocanbise(i)
+    for sentence in sentences:
+        cocanbise(sentence)
