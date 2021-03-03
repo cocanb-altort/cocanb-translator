4fd98088be64784fc2953321f74d0df8a9e6d3f1
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-03-03 15:57:18 +0100

    Update english_to_cocanb.py

--- english_to_cocanb.py
+++ english_to_cocanb.py
@@ -1,4 +1,11 @@
+
+
+def cocanbise(string):
+    initial_list = " ".split(string)
+
+
 def english_to_cocanb():
     initial_phrase = input("Please enter your English Phrase: ")
     sentences = ".".split(initial_phrase)
     for i in sentences:
+        cocanbise(i)
