8e61bce7f56e2078e79b85ee5ba20ca293e0597d
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-03-03 14:19:00 +0100

    stuff

---
+++ cocanb_to_english.py
@@ -0,0 +1,2 @@
+def cocanb_to_english():
+    print("This does not work yet")
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import functions_to_make_my_life_easier as f
 
-f.cocanb_or_english()
+def cocanb_or_english():
+    choose_lang = input("Would you like to translate Cocánb to English or English to Cocánb? Press[c/e]: ")
+    if choose_lang == "c":
+        cocanb_to_english()
+    elif choose_lang == "e":
+        english_to_cocanb()
+    else:
+        print("Please enter a valid input.")
+        cocanb_or_english()
+    input("Press enter to exit.")
+
+cocanb_or_english()
 
 input("Press enter to exit.")
