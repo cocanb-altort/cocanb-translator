ecc2ee9ae0d52ac413ae20aca7240b20c0f21101
Woojin Son <sonwoojin161@gmail.com>
2021-03-04 10:16:07 +0000

    go back

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import tkinter as tk
-
-from cocanb_to_english import *
-from english_to_cocanb import *
+from cocanb_to_english import cocanb_to_english
+from english_to_cocanb import english_to_cocanb
 
 def cocanb_or_english():
-  window = tk.Tk()
+    choose_lang = input("Would you like to translate Cocánb to English or English to Cocánb? Press[c/e]: ")
+    if choose_lang == "c":
+        cocanb_to_english()
+    elif choose_lang == "e":
+        english_to_cocanb()
+    else:
+        print("Please enter a valid input.")
+        cocanb_or_english()
+    input("Press enter to exit.")
 
 cocanb_or_english()
 
